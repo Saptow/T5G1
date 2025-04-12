7249c5d4501423f30c008bc29055d729cf96fb0d
roa7 <roystonhow@gmail.com>
2025-04-13 02:01:29 +0800

    updates to visualisation

--- apps/module4a.py
+++ apps/module4a.py
@@ -1,9 +1,10 @@
 # Module4a.py Bubble Chart 
 
-from dash import dcc, html, Input, Output, State, callback_context, get_app
+from dash import dcc, html, Input, Output, State, callback_context, get_app, ctx
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
+import numpy as np
 import dash
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
@@ -63,20 +64,14 @@ df["Balance of Trade"] = df["Trade Balance"].apply(lambda x: "Surplus" if x > 0 
 # CHANGED
 app = get_app()
 
-
-def get_filtered_countries(df, reporter_country, num_countries, order, metric):
-
-    # Step 1: Filter by reporter country
-    df = df[df["CountryA"] == reporter_country].copy()
-
-    # Step 2: Rename 'CountryB' as 'Country' for backward compatibility
+def get_filtered_countries(df, reporter_country, num_countries, order, metric, selected_year):
+    df = df[(df["CountryA"] == reporter_country) & (df["Year"] == selected_year)].copy()
     df = df.rename(columns={"CountryB": "Country"})
 
-    """Returns a list of countries based on the filter selection."""
     if order == "smallest":
         return df.nsmallest(num_countries, "Geopolitical Distance")["Country"].tolist()
     elif order == "largest":
-        if metric == "Absolute_ideal_point_distance":
+        if metric == "Geopolitical Distance":
             return df.nlargest(num_countries, "Geopolitical Distance")["Country"].tolist()
         elif metric == "Trade Deficit":
             return df.nsmallest(num_countries, "Trade Balance")["Country"].tolist()
@@ -84,13 +79,14 @@ def get_filtered_countries(df, reporter_country, num_countries, order, metric):
             return df.nlargest(num_countries, "Trade Balance")["Country"].tolist()
     return []
 
+
+
 # === Main Layout (Graph only) ===
 layout = html.Div([
 
     html.H1("Geopolitical Distance vs. Total Trade By Year"),
 
     html.Div([
-    # html.H5("Trade Distance Filters", className="text-muted mb-3"),
 
     html.Label("Select Year:"),
     dcc.Dropdown(
@@ -102,26 +98,32 @@ layout = html.Div([
         className="mb-3"
     ),
 
-    html.Label("Select Reporter Country:"),
-    dcc.Dropdown(
-        id='reporter-selector',
-        options=[{'label': country, 'value': country} for country in sorted(df["CountryA"].unique())],
-        value="Singapore",  # or any default
-        clearable=False,
-        className="mb-3"
+    html.Div([
+        html.Div([
+            html.Label("Select Reporter Country:"),
+            dcc.Dropdown(
+                id='reporter-selector',
+                options=[{'label': country, 'value': country} for country in sorted(df["CountryA"].unique())],
+                value="Singapore",
+                clearable=False,
+                style={"width": "100%"}
+            )
+        ], className="me-3", style={"width": "49%", "display": "inline-block"}),
+
+        html.Div([
+            html.Label("Select Partner Countries:"),
+            dcc.Dropdown(
+                id='country-selector',
+                options=[{'label': country, 'value': country} for country in sorted(df["CountryB"].unique())],
+                multi=True,
+                value=["China", "United States", "Malaysia", "Indonesia", "South Korea"],
+                style={"color": "black", "backgroundColor": "white", "width": "100%"}
+            )
+        ], style={"width": "49%", "display": "inline-block"}),
+    ], className="mb-3"
     ),
 
-    html.Label("Select Partner Countries:"),
-    dcc.Dropdown(
-        id='country-selector',
-        options=[{'label': country, 'value': country} for country in sorted(df["CountryB"].unique())],
-        multi=True,
-        style={"color": "black", "backgroundColor": "white"},
-        value=["China", "United States", "Malaysia", "Indonesia", "South Korea"],
-        className="mb-3"
-    ),
-
-    html.Label("View Top N Countries:"),
+    html.Label("View Top N Countries (Clear Select Partner Countries first):"),
     dcc.Slider(
         id='num-countries',
         min=0,
@@ -133,32 +135,54 @@ layout = html.Div([
         className="mb-4"
     ),
 
-    html.Label("Trade Order Criterion:"),
-    dcc.Dropdown(
-        id='order-selector',
-        options=[
-            {'label': "smallest", 'value': "smallest"},
-            {'label': "largest", 'value': "largest"}
-        ],
-        style={"color": "black", "backgroundColor": "white"},
-        clearable=True,
-        className="mb-3"
+    dbc.Row([
+    dbc.Col(
+        html.Div([
+            html.Label("Trade Order Criterion:", className="me-2"),
+            dbc.ButtonGroup(
+                [
+                    dbc.Button("Smallest", id="btn-smallest", n_clicks=0, color="secondary", outline=True),
+                    dbc.Button("Largest", id="btn-largest", n_clicks=0, color="primary", outline=True)
+                ],
+                id="order-btn-group"
+            )
+        ]),
+        width=6
     ),
-
-    html.Label("Trade Metric:"),
-    dcc.Dropdown(
-        id='metric-selector',
-        options=[
-            {'label': "geopolitical distance", 'value': "Absolute_ideal_point_distance"},
-            {'label': "trade deficit", 'value': "Trade Deficit"},
-            {'label': "trade surplus", 'value': "Trade Surplus"}
-        ],
-        style={"color": "black", "backgroundColor": "white"},
-        clearable=True,
-        className="mb-3"
+    dbc.Col(
+        html.Div([
+            html.Label("Trade Metric:", className="me-2"),
+            dbc.ButtonGroup(
+                [
+                    dbc.Button("Geopolitical Distance", id="btn-distance", n_clicks=0, color = "primary", outline=True),
+                    dbc.Button("Trade Surplus", id="btn-surplus", n_clicks=0,color = "secondary", outline=True),
+                    dbc.Button("Trade Deficit", id="btn-deficit", n_clicks=0, color = "secondary", outline=True),
+                ],
+                id="metric-btn-group"
+            ),
+            html.Small(
+                "Trade metric selection is fixed to 'Geopolitical Distance' when order is 'Smallest'",
+                className="text-muted fst-italic ms-2")
+        ], id="metric-group-div"),
+        width=6
     )
+], className="mb-3"),
+#     html.Div(
+#     "The size of the bubbles reflects reporter’s total trade value with the trading partner. "
+#     "The center point (white circle) of each bubble reflects reporter’s trade balance with the trading partner.",
+#     style={
+#         "fontSize": "0.9rem",
+#         "fontStyle": "italic",
+#         "color": "#555",
+#         "marginTop": "10px",
+#         "textAlign": "center"
+#     }
+# )
 ]),
-    # dcc.Graph(id='trade-graph'),
+    #dcc.Graph(id='trade-graph'),
+    
+    dcc.Store(id='order-selector', data='largest'),
+    dcc.Store(id='metric-selector', data='Geopolitical Distance'),
 
     html.Div(id="tab-warning4a", className="text-danger mb-2 text-center"),
 
@@ -178,31 +202,65 @@ layout = html.Div([
 
 ])
 
+### Callback to fix trade metric to Geopolitical Distance when smallest is chosen
 @app.callback(
-    Output('metric-selector', 'disabled'),
-    Output('metric-selector', 'value'),
-    Input('order-selector', 'value')
+    Output("order-selector", "data"),
+    Output("btn-smallest", "color"),
+    Output("btn-largest", "color"),
+    Output("metric-selector", "data"),
+    Output("btn-distance", "color"),
+    Output("btn-surplus", "color"),
+    Output("btn-deficit", "color"),
+    Output("btn-distance", "disabled"),
+    Output("btn-surplus", "disabled"),
+    Output("btn-deficit", "disabled"),
+    Input("btn-smallest", "n_clicks"),
+    Input("btn-largest", "n_clicks"),
+    Input("btn-distance", "n_clicks"),
+    Input("btn-surplus", "n_clicks"),
+    Input("btn-deficit", "n_clicks"),
 )
+def update_order_and_metric(n_smallest, n_largest, n_distance, n_surplus, n_deficit):
+    ctx = dash.callback_context.triggered_id
 
-def update_metric_dropdown(order):
-    if order == "smallest":
-        return True, "Absolute_ideal_point_distance"
-    return False, "-"
+    if ctx == "btn-smallest":
+        return (
+            "smallest", "primary", "secondary", 
+            "Geopolitical Distance",
+            "primary", "secondary", "secondary",  # button colors
+            True, True, True  # all metric buttons disabled
+        )
+    elif ctx == "btn-largest":
+        return (
+            "largest", "secondary", "primary", 
+            dash.no_update,
+            dash.no_update, dash.no_update, dash.no_update,
+            False, False, False  # enable buttons
+        )
 
+    # Metric button interactions only apply when order is "largest"
+    if ctx == "btn-distance":
+        return dash.no_update, dash.no_update, dash.no_update, "Geopolitical Distance", "primary", "secondary", "secondary", dash.no_update, dash.no_update, dash.no_update
+    elif ctx == "btn-surplus":
+        return dash.no_update, dash.no_update, dash.no_update, "Trade Surplus", "secondary", "primary", "secondary", dash.no_update, dash.no_update, dash.no_update
+    elif ctx == "btn-deficit":
+        return dash.no_update, dash.no_update, dash.no_update, "Trade Deficit", "secondary", "secondary", "primary", dash.no_update, dash.no_update, dash.no_update
+
+    return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
 
 @app.callback(
     Output('trade-graph', 'figure'),
     Input('reporter-selector', 'value'),
     Input('country-selector', 'value'),
     Input('num-countries', 'value'),
-    Input('order-selector', 'value'),
-    Input('metric-selector', 'value'),
+    Input('order-selector', 'data'),
+    Input('metric-selector', 'data'),
     Input('year-dropdown', 'value')
 )
 
 def update_graph(reporter_country, selected_countries, num_countries, order, metric, selected_year):
     selected_countries = set(selected_countries or [])
-    filtered_countries = set(get_filtered_countries(df, reporter_country, num_countries, order, metric))
+    filtered_countries = set(get_filtered_countries(df, reporter_country, num_countries, order, metric, selected_year))
     countries_to_display = selected_countries.union(filtered_countries)
     
     # Filter by reporter country
@@ -211,6 +269,27 @@ def update_graph(reporter_country, selected_countries, num_countries, order, met
     # Then filter by selected partner countries
     filtered_df = filtered_df[filtered_df["CountryB"].isin(countries_to_display)]
     year_df = filtered_df[filtered_df['Year'] == selected_year]
+    year_df["Trade Balance"] = year_df["Trade Balance"].round(2)
+
+    # Dynamic Y-Axis (Geopolitical Distance)
+    geo_min = min(year_df["Geopolitical Distance"].min(), filtered_df["Geopolitical Distance"].min())
+    geo_max = max(year_df["Geopolitical Distance"].max(), filtered_df["Geopolitical Distance"].max())
+    geo_padding = (geo_max - geo_min) * 0.2 if geo_max > geo_min else 0.5  # fallback padding
+
+    y_range = [geo_min - geo_padding, geo_max + geo_padding]
+
+    # Dynamic X-Axis (Trade Balance or Share)
+    x_min = year_df["Trade Balance"].min()
+    x_max = year_df["Trade Balance"].max()
+    x_padding = (x_max - x_min) * 0.2 if x_max > x_min else abs(x_max) * 0.2 or 1  # fallback padding
+
+    x_range = [x_min - x_padding, x_max + x_padding]
+
+    # Convert values to billions and round to 2 decimal places
+    year_df["Total Trade (S$ Bil)"] = (year_df["Total Trade"] / 1e9).round(2)
+    year_df["Exports (S$ Bil)"] = (year_df["Exports"] / 1e9).round(2)
+    year_df["Imports (S$ Bil)"] = (year_df["Imports"] / 1e9).round(2)
+
 
     fig = px.scatter(
         year_df,
@@ -218,7 +297,15 @@ def update_graph(reporter_country, selected_countries, num_countries, order, met
         y="Geopolitical Distance",
         size="Total Trade",
         color="CountryB",
-        hover_data={"CountryB": True, "Exports": True, "Imports": True, "Balance of Trade": True},
+        hover_data={
+            "CountryB": False,
+            "Total Trade" : False,
+            "Total Trade (S$ Bil)": ":.2f",
+            "Exports (S$ Bil)": ":.2f",
+            "Imports (S$ Bil)": ":.2f",
+            "Balance of Trade": True,
+            "Geopolitical Distance": ":.2f"
+        },
         text="CountryB",
         labels={
             "Trade Balance": "Trade Balance (S$ Bil)",
@@ -242,19 +329,6 @@ def update_graph(reporter_country, selected_countries, num_countries, order, met
             ),
             showlegend= False,
         )
-    )
-
-
-    # Add a bubble size legend at the top-right corner
-    fig.add_annotation(
-        x=year_df["Trade Balance"].max() * 1.1,  # Move it to the rightmost part
-        y=year_df["Trade Balance"].max() * 1.1,  # Position at the top
-        text="Circle Size: Total Trade Value",
-        showarrow=False,
-        font=dict(size=14, color="black"),
-        bgcolor="white",
-        bordercolor="black",
-        borderwidth=1
     )
 
     # Add a vertical reference line at x = 0 to separate surplus/deficit regions
@@ -288,11 +362,26 @@ def update_graph(reporter_country, selected_countries, num_countries, order, met
         y=-0.06
     )
 
+    fig.add_annotation(
+        xref = 'paper',
+        x = .1,
+        yref = 'paper',
+        y = 1.05,
+        text = "Bubble size: Reporter’s total trade value with trading partner. " \
+        "White circle: Reporter’s trade balance with the trading partner.",
+        showarrow=False,
+        font=dict(size=14, color="black"),
+        bgcolor="white"
+    )
+
     # Add background color for Trade Deficit region (x < 0)
     fig.add_shape(
         type="rect",
-        x0=min(0,year_df["Trade Balance"].min()*1.25), x1=0,
-        y0=0, y1=year_df["Geopolitical Distance"].max()+ 0.5,
+        x0=x_range[0], x1=0,
+        y0=0, y1=1,
+        yref="paper",  # use full height of plot
+        # x0=min(0,year_df["Trade Balance"].min()*1.25), x1=0,
+        # y0=0, y1=year_df["Geopolitical Distance"].max()+ 0.5,
         fillcolor="rgba(255, 102, 102, 0.2)",  # Light red with transparency
         layer="below",
         line_width=0
@@ -301,8 +390,11 @@ def update_graph(reporter_country, selected_countries, num_countries, order, met
     # Add background color for Trade Surplus region (x > 0)
     fig.add_shape(
         type="rect",
-        x0=0, x1=max(0,year_df["Trade Balance"].max()*1.35),
-        y0=0, y1=year_df["Geopolitical Distance"].max()+0.5,
+        x0=0, x1=x_range[1],
+        y0=0, y1=1,
+        yref="paper",
+        # x0=0, x1=max(0,year_df["Trade Balance"].max()*1.35),
+        # y0=0, y1=year_df["Geopolitical Distance"].max()+0.5,
         fillcolor="rgba(102, 204, 102, 0.2)",  # Light green with transparency
         layer="below",
         line_width=0
@@ -315,9 +407,9 @@ def update_graph(reporter_country, selected_countries, num_countries, order, met
         font=dict(size=13),
         textangle=-90,
         xref='paper',
-        x=-0.04,
+        x=-0.045,
         yref='paper',
-        y=.025
+        y=.02
     )
     
     # Add annotation for "Further Politically" (above average distance)
@@ -327,27 +419,29 @@ def update_graph(reporter_country, selected_countries, num_countries, order, met
         font=dict(size=13),
         textangle=-90,
         xref='paper',
-        x=-0.04,
+        x=-0.045,
         yref='paper',
-        y=1.03
+        y=1.01
     )
     
     # Customize layout
-    fig.update_traces(textposition="top center")
+    fig.update_traces(
+        textposition="top center",
+    )
     
     fig.update_layout(
     width=1250,
     height=700,
     xaxis=dict(
         title=dict(text="Trade Balance (S$ Bil)", font=dict(size=18)),
-        range=(year_df["Trade Balance"].min() * 1.25 , year_df["Trade Balance"].max()*1.25)
+        range=x_range
     ),
     yaxis=dict(
         title=dict(text="Geopolitical Distance", font=dict(size=18)),
-        range=[0, year_df["Geopolitical Distance"].max() + 0.5]
+        range=y_range
     ),
     plot_bgcolor="white",
-    showlegend=True
+    showlegend=False
     )
 
     return fig
